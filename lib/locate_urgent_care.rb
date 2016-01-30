5cc9b11f5d9518c1c4150cde87b84efaea0e80db
marcotsui2003 <marcotsui2003@yahoo.com>
2016-01-29 20:07:00 -0500

    CLi class done

--- lib/locate_urgent_care.rb
+++ lib/locate_urgent_care.rb
@@ -98,10 +98,81 @@ class LocateUrgentCare::Clinic
     clinics_array.each do |clinic_hash|
       self.new(clinic_hash)
     end
-    self.all
   end
 
 end
 
 
+class LocateUrgentCare::CommandLineInterface
 
+  attr_accessor :zip_code, :category_id
+
+  def run
+    get_user_input
+    find_clinics
+    display_clinic_names
+  end
+
+  def get_user_input
+    puts "Welcome to Locate Urgent Care."
+    get_zip_code
+    get_category_id
+  end
+
+  def get_zip_code
+    puts "Please enter your zip code:"
+    @zip_code = gets.chomp.to_i
+  end
+
+    
+
+  def get_category_id
+    puts "Please select from the following categories:"
+    puts "1.Urgent Care."
+    puts "2.Occuptaional Medicine."
+    puts "3.Primary Care."
+    puts "4.Pediatric Urgent Care."
+    puts "5.Retail Clinic."
+    puts "Input 1-5:"
+    @category_id = gets.chomp.to_i
+    get_category_id if !(1..5).include?(@category_id)
+  end
+
+  def find_clinics
+    clinic_pages = LocateUrgentCare::Scraper.scrape_clinic_pages(@zip_code,@category_id)
+    clinics_array = LocateUrgentCare::Scraper.scrape_clinic_info(clinic_pages)
+    LocateUrgentCare::Clinic.create_clinics(clinics_array)
+  end
+
+  def display_clinic_names
+    num = LocateUrgentCare::Clinic.all.size
+    puts "We found #{num} clinics near you zip codes open now:"
+    if num > 0
+      (1..num).each do |i|
+        puts "#{i}. #{LocateUrgentCare::Clinic.all[i-1].name}."
+      end
+      return_clinic_info
+    end
+  end
+
+  def return_clinic_info
+    num = LocateUrgentCare::Clinic.all.size
+    puts "Which clinic you want to look into further? (1- #{num}):"
+    input = gets.chomp.to_i
+    return_clinic_info if !(1..num).include?(input)
+
+    clinic = LocateUrgentCare::Clinic.all[input-1]
+    puts "Name      : #{clinic.name}"
+    puts "Tel       : #{clinic.tel}"
+    puts "Distance  : #{clinic.distance}"
+    puts "url       : #{clinic.url}"
+    puts "rating    : #{clinic.rating}"
+    puts "directions: #{clinic.directions}"
+
+    puts "Do you want to look at another clinic?(Y/N)"
+    input1 = gets.chomp.to_s.upcase
+    return_clinic_info if input1 == "Y"
+    
+  end
+
+end
